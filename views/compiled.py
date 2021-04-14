80a5dd0a8802f01a16479e80bd5c7a1ce21891e1
Kyle Brim <56034457+kylebrim@users.noreply.github.com>
2021-04-14 11:28:29 -0500

    Works with JSON files now
    Delete the block between the comments whenever you are ready to get the JSON and removed the # in Lines 9 and 10.

--- views/compiled.py
+++ views/compiled.py
@@ -1,56 +1,37 @@
 import math
-import seaborn as sns
-import matplotlib.pyplot as plt
+import json
 from flask import Flask, redirect, url_for, render_template
-
-# patients = [['324009725', 'Kyle', 'Brim', '21 Aug 1997', 'Male',
-# 			 '177 lbs', 'McKinney, TX'],
-# 			['123001234', 'Yazan', 'Abuashour', '1 Jan 1990',
-# 			 'Non-binary', '365 lbs', 'Compton, CA'],
-# 			['456009898', 'Lauren', 'Yamthe', '31 Oct 1969',
-# 			 'Female', '112 lbs', 'Houston, TX'],
-# 			['909009009', 'Haani', 'Tai', '25 Dec 2000',
-# 			 'Gender Fluid', '225 lbs', 'New York, NY'],
-# 			['123009999', 'Hannah', 'McKinley', '31 Oct 2020',
-# 			 'Mustang', '7 lbs', 'Tampa, FL'],
-# 			['888008888', 'Michael', 'Guillen', '29 Feb 1999',
-# 			 'Often', '800 lbs', 'City, ST']]
-patients = [['324009725', 'Kyle Brim', '110 Waverly Dr',
-			 '2145514496', '21 Aug 1997', 'True', 'Male',
-			 'Single', 'White'],
-			 ['123001234', 'Yazab Abuashour', '1600 Pennsylvania Ave',
-			  '8320001111', '1 Jan 1900', 'False', 'Shemale',
-			  'Polyamorous', 'French']]
-length = len(patients)
-
-test_cl = [[33, 32, 31, 36, 37, 36, 32, 31, 30, 30, 33],
-		   [30, 30, 30, 30, 31, 31, 32, 32, 33, 33, 33]]
-# m = []
-# for c in range(len(test_cl)):
-# 	m.append(c)
-
-# fig = sns.lineplot(x=m, y=test_cl,
-# 	               color='blue')
-# fig.set_ylim(0, 40)
 
 app = Flask(__name__)
 
 @app.route("/")
 def login():
-	data = request.json
-	return render_template("login.html")
+	# data = request.json
+	return render_template("login.html") #, data
+
+### Can delete this block after JSON is fed in ###
+file = "templates/dj.json"
+
+with open(file) as f:
+	data = json.loads(f.read())
+### Can delete this block after JSON is fed in ###
+
+def sortFunction(value):
+	return value['name']
+
+data = sorted(data, key=sortFunction)
+length = len(data)
+j = []
+for k in range(len(data)):
+	j.append(len(data[k]['measurements']))
 
 @app.route("/patient")
-	return render_template("login.html")
-
-@app.route("/home")
 def home():
-	return render_template("index.html", id=patients, l=length)
+	return render_template("index.html", data=data, l=length, j=j)
 
-@app.route("/patient/<id>")
-def id_page(id):
-	return render_template("patient.html", id=patients[int(id)], sp=id, data=test_cl[int(id)])
+@app.route("/patient/<num>")
+def id_page(num):
+	return render_template("patient.html", data=data[int(num)])
 
 if __name__ == "__main__":
 	app.run(debug=True)
-
